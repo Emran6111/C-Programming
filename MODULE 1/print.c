58f2074b3f5b590154ac7cfeee3040c1d4ca89fa
EMRAN MAHMUD <emranmahmud@EMRANs-MacBook-Air.local>
2023-04-04 00:36:16 +0600

    Practice day 1

---
+++ MODULE 1/print.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    printf("Hello world\tNow I am a programmer");
+    return 0;
+}
+
+//\n- new line
+//\t- tab
+// Hello world
+// Now I am a programmer
+//New line e jao
---
+++ MODULE 1/variable.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int rahim=10;
+    float korim=10.5;
+    char habib='Z';
+    printf("%d %.2f %c",rahim, korim, habib);
+    return 0;
+}
